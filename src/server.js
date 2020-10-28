3a785b6b1c525deeb3eb7362eea04fbdab363700
Herbert Paes <herbert.pbarreto@gmail.com>
2020-10-28 14:24:16 -0300

    Render products on browser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const http = require("http");
+const products = require("./data/products.json");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, ("Content-Type", "application/json"));
+  res.end(JSON.stringify(products));
+});
 
 const PORT = process.env.PORT || 5000;
 
