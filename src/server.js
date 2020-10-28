28f07da667fc893b074ef1ed203ab4d000881e14
Herbert Paes <herbert.pbarreto@gmail.com>
2020-10-28 14:28:26 -0300

    Add route and method checking to the server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const http = require("http");
 const products = require("./data/products.json");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, ("Content-Type", "application/json"));
-  res.end(JSON.stringify(products));
+  if (req.url === "/api/products" && req.method === "GET") {
+    res.writeHead(200, ("Content-Type", "application/json"));
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(400, ("Content-Type", "application/json"));
+    res.end(JSON.stringify("Route not found!"));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
